10e3738af6da58bcfdbf2b55a1b11963ca740ba4
Eugene O'Fallon <epofallon@gmail.com>
2021-02-19 19:00:18 +0000

    begin updating RPS game

--- RB101_L2_Small_Programs/rock_paper_scissors.rb
+++ RB101_L2_Small_Programs/rock_paper_scissors.rb
@@ -1,21 +1,27 @@
-VALID_CHOICES = { r: 'rock',
-                  p: 'paper',
-                  c: 'scissors',
-                  l: 'lizard',
-                  s: 'spock' }
+VALID_CHOICES = { r: 'rock', p: 'paper', c: 'scissors', l: 'lizard', s: 'spock' }
 
-WHO_BEATS_WHO = { rock: ['lizard', 'scissors'],
-                  paper: ['rock', 'spock'],
-                  scissors: ['paper', 'lizard'],
-                  lizard: ['spock', 'paper'],
-                  spock: ['scissors', 'rock'] }
+WHO_BEATS_WHO = { rock: ['lizard', 'scissors'], paper: ['rock', 'spock'], scissors: ['paper', 'lizard'], lizard: ['spock', 'paper'], spock: ['scissors', 'rock'] }
+
+GOAL_WINS = 5
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-def display_choices
-  VALID_CHOICES.each { |k, v| Kernel.puts("    #{k} -> #{v}") }
+def get_choice
+  choice = ''
+  loop do
+    prompt("Choose one:")
+    VALID_CHOICES.each { |k, v| Kernel.puts("    #{k} -> #{v}") }
+    choice = Kernel.gets().chomp().to_sym()
+    break if valid_choice?(choice)
+    prompt("That's not a valid choice.")
+  end
+  VALID_CHOICES[choice]
+end
+
+def valid_choice?(choice)
+  VALID_CHOICES.include?(choice)
 end
 
 def win?(first, second)
@@ -32,48 +38,38 @@ def display_results(player, computer)
   end
 end
 
+def update_score(choice1, choice2)
+  win?(choice1, choice2) ? 1 : 0
+end
+
 prompt("Welcome to Rock, Paper, Scissor, Lizard, Spock!")
 prompt("First to 5 points wins the game!")
 
 player_score = 0
 computer_score = 0
 loop do
-  choice = ''
-  loop do
-    prompt("Choose one:")
-    display_choices
-    choice = Kernel.gets().chomp().to_sym()
-
-    if VALID_CHOICES.include?(choice)
-      break
-    else
-      prompt("That's not a valid choice.")
-    end
-  end
-
+  choice = get_choice
+  
   comp_choice = VALID_CHOICES.values.sample
 
-  prompt("You chose: #{VALID_CHOICES[choice]}; Computer chose: #{comp_choice}")
+  prompt("You chose: #{choice}; Computer chose: #{comp_choice}")
 
-  display_results(VALID_CHOICES[choice], comp_choice)
-
-  if win?(VALID_CHOICES[choice], comp_choice)
-    player_score += 1
-  elsif win?(comp_choice, VALID_CHOICES[choice])
-    computer_score += 1
-  end
+  display_results(choice, comp_choice)
+  player_score += update_score(choice, comp_choice)
+  computer_score += update_score(comp_choice, choice)
 
   prompt("Your score: #{player_score}; Computer score: #{computer_score}")
-
-  # prompt("Do you want to play again?")
-  # answer = Kernel.gets().chomp()
-  if player_score == 5
+  
+  if player_score == GOAL_WINS
     prompt("You've won! Congratulations!")
     break
-  elsif computer_score == 5
+  elsif computer_score == GOAL_WINS
     prompt("The computer won. Better luck next time!")
     break
   end
+  break if goal_reached?(player_score,computer_score)
 end
+
+
 
 prompt("Thank you for playing. Good bye!")
