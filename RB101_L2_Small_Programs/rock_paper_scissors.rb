2a0d21fa2cb696f2506910534002c3e4bba0bc7f
Eugene O'Fallon <epofallon@gmail.com>
2021-02-21 02:12:19 +0000

    finished RPSLS game for code review

--- RB101_L2_Small_Programs/rock_paper_scissors.rb
+++ RB101_L2_Small_Programs/rock_paper_scissors.rb
@@ -1,79 +1,165 @@
 require 'yaml'
 PROMPTS = YAML.load_file('rps_messages.yml')
 
-VALID_CHOICES = { r: 'rock', p: 'paper', c: 'scissors', l: 'lizard', s: 'spock' }
-WHO_BEATS_WHO = { rock: ['lizard', 'scissors'], paper: ['rock', 'spock'], scissors: ['paper', 'lizard'], lizard: ['spock', 'paper'], spock: ['scissors', 'rock'] }
+VALID_CHOICES = { r: 'rock',
+                  p: 'paper',
+                  sc: 'scissors',
+                  l: 'lizard',
+                  sp: 'spock' }
+
+WHO_BEATS_WHO = { rock: ['lizard', 'scissors'],
+                  paper: ['rock', 'spock'],
+                  scissors: ['paper', 'lizard'],
+                  lizard: ['spock', 'paper'],
+                  spock: ['scissors', 'rock'] }
+
 GOAL_WINS = 5
+
 def messages(string)
   PROMPTS[string]
 end
 
 def prompt(message)
-  Kernel.puts("=> #{PROMPTS[message]}")
+  Kernel.puts("=> #{messages(message)}")
 end
 
-def get_choice
+def clear
+  system("clear") || system("cls")
+end
+
+def welcome
+  clear
+  prompt 'welcome'
+  sleep(0.5)
+  prompt 'goal'
+  sleep(0.5)
+  display_rules
+  clear
+end
+
+def display_rules
+  answer = ''
+  loop do
+    prompt "display_rules?"
+    answer = Kernel.gets().chomp().downcase
+    break if valid_response?(answer)
+    prompt 'invalid_choice'
+  end
+
+  if answer == 'y'
+    prompt 'spacer'
+    WHO_BEATS_WHO.each { |k, v| puts "   ~ #{k} beats #{v[0]} & #{v[1]}" }
+    prompt 'spacer'
+    prompt 'any_key'
+    Kernel.gets().chomp()
+  end
+end
+
+def valid_response?(answer)
+  %w(y n).include? answer
+end
+
+def get_choice(choices)
   choice = ''
   loop do
+    prompt 'spacer'
     prompt 'choose'
     VALID_CHOICES.each { |k, v| Kernel.puts("    #{k} -> #{v}") }
-    choice = Kernel.gets().chomp().to_sym()
+    choice = Kernel.gets().chomp().downcase().to_sym()
     break if valid_choice?(choice)
     prompt 'invalid_choice'
   end
-  VALID_CHOICES[choice]
+  choices[:player] = VALID_CHOICES[choice]
+  clear
 end
 
 def valid_choice?(choice)
   VALID_CHOICES.include?(choice)
 end
 
+def get_comp_choice(choices)
+  choices[:computer] = VALID_CHOICES.values.sample
+end
+
 def win?(first, second)
   WHO_BEATS_WHO[first.to_sym].include?(second)
 end
 
-def display_results(player, computer)
-  if win?(player, computer)
+def display_choices(choices)
+  prompt 'spacer'
+  puts "=> #{messages('you_chose')} #{choices[:player]}"
+  puts "=> #{messages('comp_chose')} #{choices[:computer]}"
+  prompt 'spacer'
+  sleep(0.75)
+end
+
+def display_result(choices)
+  if win?(choices[:player], choices[:computer])
     prompt 'you_won_round'
-  elsif win?(computer, player)
+  elsif win?(choices[:computer], choices[:player])
     prompt 'comp_won_round'
   else
     prompt 'tie_round'
   end
+  prompt 'spacer'
 end
 
-def update_score(choice1, choice2)
-  win?(choice1, choice2) ? 1 : 0
+def update_score(choices, scores)
+  if win?(choices[:player], choices[:computer])
+    scores[:player] += 1
+  elsif win?(choices[:computer], choices[:player])
+    scores[:computer] += 1
+  end
 end
 
-def goal_reached?(player, computer)
-  player == GOAL_WINS || computer == GOAL_WINS
+def display_score(scores)
+  puts "=> #{messages('your_score')} #{scores[:player]}"
+  puts "=> #{messages('comp_score')} #{scores[:computer]}"
+  sleep(2.5)
+  clear
 end
 
-def display_winner(player, computer)
-  prompt player == GOAL_WINS ? 'you_won_game' : 'comp_won_game'
+def goal_reached?(scores)
+  scores.value?(GOAL_WINS)
 end
 
-prompt 'welcome'
-prompt 'goal'
+def display_winner(scores)
+  prompt 'spacer'
+  prompt scores[:player] == GOAL_WINS ? 'you_won_game' : 'comp_won_game'
+  prompt 'spacer'
+  sleep(0.75)
+end
 
-player_score = 0
-computer_score = 0
+def play_again?
+  answer = ''
+  loop do
+    prompt 'play_again?'
+    answer = Kernel.gets().chomp().downcase
+    break if valid_response?(answer)
+    prompt 'invalid_choice'
+  end
+  answer == 'y'
+end
+
+def reset_scores(scores)
+  scores.transform_values! { 0 }
+end
+
+welcome
+scores = { player: 0, computer: 0 }
+choices = { player: '', computer: '' }
 loop do
-  choice = get_choice
-  comp_choice = VALID_CHOICES.values.sample
-
-  prompt "You chose: #{choice}; Computer chose: #{comp_choice}"
-
-  display_results(choice, comp_choice)
-  player_score += update_score(choice, comp_choice)
-  computer_score += update_score(comp_choice, choice)
-  
-  prompt "Your score: #{player_score}; Computer score: #{computer_score}"
-  
-  break if goal_reached?(player_score,computer_score)
+  get_choice(choices)
+  get_comp_choice(choices)
+  display_choices(choices)
+  display_result(choices)
+  update_score(choices, scores)
+  display_score(scores)
+  next unless goal_reached?(scores)
+  display_winner(scores)
+  break unless play_again?
+  reset_scores(scores)
+  clear
 end
-
-display_winner(player_score,computer_score)
 
 prompt 'good_bye'
