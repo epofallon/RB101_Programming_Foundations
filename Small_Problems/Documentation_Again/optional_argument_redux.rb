335a784bdb2a0e002717dd6684599272b30d4b3d
Eugene O'Fallon <epofallon@gmail.com>
2021-02-23 02:37:53 +0000

    updated with tinkering

--- Small_Problems/Documentation_Again/optional_argument_redux.rb
+++ Small_Problems/Documentation_Again/optional_argument_redux.rb
@@ -3,5 +3,5 @@ require 'date'
 puts Date.civil
 puts Date.civil(2016)
 puts Date.civil(2016, 5)
-puts Date.civil(2016, 5, 13)
-puts Date.civil(1751, 5, 13, Date::ENGLAND)
+puts Date.civil(2016, 5, -31)
+puts Date.civil(1751, 5, -32)
