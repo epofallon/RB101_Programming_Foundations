e439640d072a82a2afc905781e75bfa64440f265
Eugene O'Fallon <epofallon@gmail.com>
2021-03-16 01:43:03 +0000

    add pedac_process notes and another Easy_6 problem

--- Small_Problems/Easy_6/reversed_arrays_2.rb
+++ Small_Problems/Easy_6/reversed_arrays_2.rb
@@ -43,3 +43,28 @@
     - return 'rev_arr'
 =end
 
+def reverse(arr)
+  rev_arr = []
+  return rev_arr if arr == []
+  forwards_i = 0
+  backwards_i = arr.length - 1
+  
+  loop do
+    rev_arr[forwards_i] = arr[backwards_i]
+    break if rev_arr.length == arr.length
+    forwards_i += 1
+    backwards_i -= 1
+  end
+  rev_arr
+end
+
+puts reverse([1,2,3,4]) == [4,3,2,1]          # => true
+puts reverse(%w(a b e d c)) == %w(c d e b a)  # => true
+puts reverse(['abc']) == ['abc']              # => true
+puts reverse([]) == []                        # => true
+
+list = [1, 3, 2]                      # => [1, 3, 2]
+new_list = reverse(list)              # => [2, 3, 1]
+puts list.object_id != new_list.object_id  # => true
+puts list == [1, 3, 2]                     # => true
+puts new_list == [2, 3, 1]                 # => true
