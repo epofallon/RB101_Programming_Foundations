a3a4145753c996d80ed49e0096c0fde7fccf8c52
Eugene O'Fallon <epofallon@gmail.com>
2021-04-07 18:16:27 +0000

    added 5 round gameplay and script messages to ttt

--- RB101_L6_Slightly_Larger_Programs/tic_tac_toe.rb
+++ RB101_L6_Slightly_Larger_Programs/tic_tac_toe.rb
@@ -1,20 +1,45 @@
 require 'pry'
+require 'yaml'
 
+PROMPTS = YAML.load_file('ttt_messages.yml')
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
                 [[1, 5, 9], [3, 5, 7]] # diagonals
+GOAL_WINS = 5
+
+def messages(string)
+  PROMPTS[string]
+end
 
 def prompt(msg)
-  puts "=> #{msg}"
+  puts messages(msg) ? "=> #{messages(msg)}" : "=> #{msg}"
+end
+
+def valid_response?(answer)
+  %w(y n).include? answer
+end
+
+def clear
+  system("clear") || system("cls")
+end
+
+def welcome
+  clear
+  prompt 'welcome'
+  sleep(1)
+  prompt 'goal'
+  sleep(1)
+  #display_rules
+  clear
 end
 
 # rubocop:disable Metrics/AbcSize
 def display_board(brd)
   system 'clear'
-  prompt "Player: #{PLAYER_MARKER}. Computer: #{COMPUTER_MARKER}."
+  prompt "Player: '#{PLAYER_MARKER}'. Computer: '#{COMPUTER_MARKER}'."
   puts ""
   puts "     |     |   "
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -58,7 +83,7 @@ def player_places_piece!(brd)
     prompt "Choose a square (#{joinor(empty_squares(brd))}):"
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
-    prompt "Sorry, that's not a valid number"
+    prompt 'invalid_number'
   end
   brd[square] = PLAYER_MARKER
 end
@@ -90,6 +115,45 @@ end
 def update_score(winner, scores)
   scores[winner] += 1 #unless scores[winner].nil?
 end
+
+def display_score(scores)
+  puts "=> #{messages('your_score')} #{scores['Player']}"
+  puts "=> #{messages('comp_score')} #{scores['Computer']}"
+  sleep(2.5)
+  clear
+end
+
+def goal_reached?(scores)
+  scores.value?(GOAL_WINS)
+end
+
+def grand_winner(scores)
+  scores['Player'] == GOAL_WINS ? :player : :computer
+end
+
+def display_winner(champ)
+  prompt 'spacer'
+  prompt champ == :player ? 'you_won_game' : 'comp_won_game'
+  prompt 'spacer'
+  sleep(0.75)
+end
+
+def play_again?
+  answer = ''
+  loop do
+    prompt 'play_again?'
+    answer = Kernel.gets().chomp().downcase
+    break if valid_response?(answer)
+    prompt 'invalid_choice'
+  end
+  answer == 'y'
+end
+
+def reset_scores(scores)
+  scores.transform_values! { 0 }
+end
+
+welcome
 scores = { 'Player'=>0, 'Computer'=>0 }
 loop do
   board = initialize_board
@@ -111,12 +175,14 @@ loop do
     prompt "#{winner} won!"
     update_score(winner,scores)
   else
-    prompt "It's a tie!"
+    prompt 'tie_round'
   end
-  prompt "Player: #{scores['Player']}; Computer: #{scores['Computer']}"
-  prompt "Play again? (y or n)"
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  display_score(scores)
+  next unless goal_reached?(scores)
+  champ = grand_winner(scores)
+  display_winner(champ)
+  break unless play_again?
+  reset_scores(scores)
 end
 
-prompt "Thanks for playing Tic-Tac-Toe! Goodbye!"
+prompt 'good_bye'
