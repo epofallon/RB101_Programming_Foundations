78331caf37e68d62770728002e7a5647859d9d44
Eugene O'Fallon <epofallon@gmail.com>
2021-04-22 18:29:57 +0000

    add abc.rb

--- Small_Problems/Medium_2/abc.rb
+++ Small_Problems/Medium_2/abc.rb
@@ -22,6 +22,33 @@
 # Examples / Test Cases
 =begin
   block_word?('BATCH') == true
-  block_word?('BUTCH') == false
+  block_word?('BUTCH') == false #=> U & H are the same block
   block_word?('jest') == true
 =end
+
+# Data Structure
+=begin
+  - [['b', 'o'], ['x', 'k'], ['d', 'q'], ['c', 'p'], ['n', 'a'], ['g', 't'], ['r', 'e'], ['f', 's'], ['j', 'w'], ['h', 'u'], ['v', 'i'], ['l', 'y'], ['z', 'm']]
+  - an array of 'block' arrays
+=end
+
+# Algorithm
+=begin
+  - loop through array of blocks
+    - count number of times side_a appears in string
+    - count number of times side_b appears in string
+    - sum total counts
+    - return 'false' if sum > 1
+  - return 'true' if iterations complete
+=end
+BLOCKS = ['bo', 'xk', 'dq', 'cp', 'na', 'gt', 're', 'fs', 'jw', 'hu', 'vi', 'ly', 'zm']
+require 'pry'
+
+def block_word?(word)
+  word = word.downcase
+  BLOCKS.all? { |block| word.count(block) < 2 }
+end
+p block_word?('BATCH') 
+p block_word?('BUTCH') 
+p block_word?('jest') 
+p block_word?('starkl')
