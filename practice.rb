47a796b2f260cfedce5e2f46b1831a613b535971
Eugene O'Fallon <epofallon@gmail.com>
2021-05-19 18:09:26 +0000

    update pratice

--- practice.rb
+++ practice.rb
@@ -1,7 +1,17 @@
-hello = 'hi'
+UUID_OPTIONS = ('0'..'9').to_a + ('a'..'f').to_a
 
-def hello
-  "Saying hello!"
+def uuid_gen
+  lengths = [8, 4, 4, 4, 12]
+  uuid = []
+  
+  lengths.each do |length|
+    strng = ''
+    length.times { strng << UUID_OPTIONS.sample }
+    uuid << strng
+  end
+  
+  uuid.join('-')
 end
 
-puts hello
+p uuid_gen
+p "f65c57f6-a6aa-17a8-faa1-a67f2dc9fa91"
